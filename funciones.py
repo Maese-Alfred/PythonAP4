7ba2d5b51dc0d15a3912843bbacdacadddf6691d
Maese-Alfred <luis_pinzon82232@elpoli.edu.co>
2025-06-05 17:08:36 -0500

    correcion al counting sort

--- funciones.py
+++ funciones.py
@@ -35,15 +35,26 @@ def buscarMateriaBinaria(materias_ordenadas, nombre):
 
 def ordenarPorCountingSort(linked_list):
     materias = list(linked_list.recorrer())
-    if not materias:
-        return LinkedList()
-    max_calificacion = max(m.calificacion for m in materias)
-    count = [0] * (max_calificacion + 1)
-    for m in materias:
-        count[m.calificacion] += 1
+    max_val = 100
+    min_val = 1
+    rango = max_val - min_val + 1
+    conteo = [0] * rango
+
+    for num in materias:
+        conteo[num - min_val] += 1
+
+    for i in range(1, rango):
+        conteo[i] += conteo[i - 1]
+
+    salida = [0] * len(materias)
+
+    for num in reversed(materias):
+        salida[conteo[num - min_val] - 1] = num
+        conteo[num - min_val] -= 1
+
+    for i in range(len(materias)):
+        materias[i] = salida[i]
     ordenadas = LinkedList()
-    for cal in range(max_calificacion + 1):
-        for m in materias:
-            if m.calificacion == cal:
-                ordenadas.insertarAlFinal(m)
+    for materia in materias:
+        ordenadas.insertarAlFinal(materia)
     return ordenadas
